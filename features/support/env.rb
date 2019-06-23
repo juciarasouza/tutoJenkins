30bd38bb7d46f5bb25f539f2883e2b77d313e70d
Juciara Nepomuceno <juciara@Juciara.local>
2019-06-23 14:13:48 +0200

    test

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,7 +15,18 @@ when "firefox"
 when "chrome"
   @driver = :selenium_chrome
 when "headless"
-  @driver = :selenium_chrome_headless
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument  "--headless"
+      options.add_argument  "--disable-gpu" 
+      options.add_argument  "--no-sandbox"
+      options.add_argument  "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+   
+  @driver =  :selenium_chrome_headless
+  
 else
   puts "Invalid browser"
 end
